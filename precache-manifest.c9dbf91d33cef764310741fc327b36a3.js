1b78d5ab2d798cf8939466d0686e21fd70027a46
KrazhauRaman <krazhau.raman@gmail.com>
2018-10-26 11:01:06 +0300

    Updates

---
+++ precache-manifest.c9dbf91d33cef764310741fc327b36a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afc02563581fe9a95d50",
+    "url": "/photo_view/static/js/runtime~main.afc02563.js"
+  },
+  {
+    "revision": "f826b3b72b5895096259",
+    "url": "/photo_view/static/js/main.f826b3b7.chunk.js"
+  },
+  {
+    "revision": "f9cd12e12c927a9d57f9",
+    "url": "/photo_view/static/js/1.f9cd12e1.chunk.js"
+  },
+  {
+    "revision": "f826b3b72b5895096259",
+    "url": "/photo_view/static/css/main.87c5bd78.chunk.css"
+  },
+  {
+    "revision": "ecec2d905f3b6794ef7daec07754cae8",
+    "url": "/photo_view/index.html"
+  }
+];
